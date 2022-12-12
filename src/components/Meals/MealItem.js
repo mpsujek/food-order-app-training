fc26d51f96beafdfc2cffaf6faf20ac8a68b775f
mpsujek <mateusz.psujek@gmail.com>
2022-12-12 22:20:40 +0100

    :Single meal component

---
+++ src/components/Meals/MealItem.js
@@ -0,0 +1,15 @@
+import classes from './MealItem.module.css';
+
+const MealItem = ({ id, name, description, price }) => {
+  const priceWithDolar = `$${price.toFixed(2)}`;
+  return (
+    <li key={id}>
+      <div className={classes.meal}>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{priceWithDolar}</div>
+      </div>
+    </li>
+  );
+};
+export default MealItem;
