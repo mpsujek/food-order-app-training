1c6d5624d133ea09f59a258fb9dce5d57d73d9a7
mpsujek <mateusz.psujek@gmail.com>
2022-12-13 11:10:04 +0100

    Input component added

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -4,8 +4,8 @@ import classes from './MealItem.module.css';
 const MealItem = ({ id, name, description, price }) => {
   const priceWithDolar = `$${price.toFixed(2)}`;
   return (
-    <li key={id}>
-      <div className={classes.meal}>
+    <li key={id} className={classes.meal}>
+      <div>
         <h3>{name}</h3>
         <div className={classes.description}>{description}</div>
         <div className={classes.price}>{priceWithDolar}</div>
