4ffd245d5e5eaab1e13e29ddb603ad7cbb0615ee
mpsujek <mateusz.psujek@gmail.com>
2022-12-22 15:33:55 +0100

    Error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,10 +6,14 @@ import MealItem from './MealItem';
 const AvailableMeals = () => {
   const [mealsArray, setMealsArray] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(process.env.REACT_APP_MEALS_URL);
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
       const responseData = await response.json();
       const loadedMeals = [];
       for (const key in responseData) {
@@ -20,11 +24,17 @@ const AvailableMeals = () => {
           price: responseData[key].price,
         });
       }
+
       setIsLoading(false);
       setMealsArray(loadedMeals);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
   const mealsList = mealsArray.map((meal) => (
     <MealItem
       key={meal.id}
@@ -37,6 +47,7 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
+        {httpError && <p>{httpError}</p>}
         {isLoading && <p>Loading...</p>}
         <ul>{mealsList}</ul>
       </Card>
